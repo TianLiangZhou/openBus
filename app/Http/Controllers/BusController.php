208f2984e28243af848b30961946c1a148f316b3
zhoutl <mfkgdyve@gmail.com>
2016-06-30 18:29:48 +0800

    添加控制器

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -1,15 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zhoutianliang
- * Date: 2016/6/30
- * Time: 17:51
- */
 
 namespace App\Http\Controllers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
 
+
+/**
+ * Class BusController
+ * @author zhoutianliang <mfkgdyve@gmail.com>
+ * @package App\Http\Controllers
+ */
 class BusController
 {
-
+    public function receive(Request $request, Response $response)
+    {
+        
+    }
 }
