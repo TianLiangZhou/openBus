8ca658d5c6d94be0cd6be388abc0e7f1ff3c5dac
zhoutl <mfkgdyve456@126.com>
2016-07-03 00:57:02 +0800

    modify config file

--- config/app.php
+++ config/app.php
@@ -9,10 +9,7 @@
 return [
     'debug' => true,
     'weixin' => [
-        'appid' => 'wx51abd28aef9dc1d8',
-        'secret'=> '50d32e66c8b29a2c97eb2db8d99ab119',
-        'token' => '5e13a7286238398f05b05cbf81142b61',
-        'url' => 'https://api.weixin.qq.com/cgi-bin/',
+
     ],
     'cache' => [
         'driver' => 'file',
