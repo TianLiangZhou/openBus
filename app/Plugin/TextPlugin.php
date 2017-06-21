6e072cb539e438e833691413e79a4239085dd38f
meshell <mfkgdyve@gmail.com>
2017-06-21 21:29:55 +0800

    添加空格格式

--- app/Plugin/TextPlugin.php
+++ app/Plugin/TextPlugin.php
@@ -20,7 +20,7 @@ class TextPlugin implements ResponsePluginInterface
     private $defaultSplit = [
         '-', '_', '?', '|',
         '$', '#', '@', '&',
-        '%', '~'
+        '%', '~', ' '
     ];
 
     private $config = [];
@@ -29,7 +29,7 @@ class TextPlugin implements ResponsePluginInterface
 无法识别此消息(查询须知)\n
 公交线路如: 37, 37路, 1号线, 37_杭州, 37_广州, 地铁(如: 1号线，4号线，6号线_广州)\n
 换乘查询如: 起点_终点, 起点_终点_城市, 如(文一路_西湖文化广场)\n
-分隔符可以为: -, _, ?, |, $, #, @, &, %, ~。如(37|广州)
+分隔符可以为: -, _, ?, |, $, #, @, &, %, ~, 空格。如(37|广州)
 EOF;
 
     private $defaultCity = '杭州';
