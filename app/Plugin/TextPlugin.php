49a387341f629bd3ecc6f7dab8afd2623cc187d9
zhoutianliang <zhoutianliang@bianfeng.com>
2017-11-20 14:27:22 +0800

    添加一批符号分隔

--- app/Plugin/TextPlugin.php
+++ app/Plugin/TextPlugin.php
@@ -20,7 +20,9 @@ class TextPlugin implements ResponsePluginInterface
     private $defaultSplit = [
         '-', '_', '?', '|',
         '$', '#', '@', '&',
-        '%', '~'
+        '%', '~', '/', '%',
+        '^', '*', '=', '+',
+        ' '
     ];
 
     private $config = [];
