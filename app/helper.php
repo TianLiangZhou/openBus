2bd5ad1314b237ce8c4b3485552bd74e6c31d1a8
zhoutianliang <mfkgdyve@gmail.com>
2021-02-06 21:26:09 +0800

    delete h5

--- app/helper.php
+++ app/helper.php
@@ -2,6 +2,11 @@
 
 use App\Support\Env;
 
+/**
+ * @param string $key
+ * @param null $default
+ * @return mixed
+ */
 function env(string $key, $default = null)
 {
     return Env::get($key, $default);
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  gateway: 'https://openbus.loocode.com',
-  name: "公交通",
-};
